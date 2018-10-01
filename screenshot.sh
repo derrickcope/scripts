21b786e733fcd57b721908c0d916affbee3ba78e
derrickcope <derrick@thecopes.me>
2018-10-01 10:08:08 +0800

    October 2018

--- screenshot.sh
+++ screenshot.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 DATE=`date +%Y-%m-%d\ %H:%M:%S`
-import -window root -crop 3840x1080 "$HOME/pictures/screenshot/screenshot $DATE.png"
+import -window root -crop 955x540 "$HOME/pictures/screenshot/screenshot $DATE.png"
